cd67d8019602c54f89104c2e4f69ab94707dea19
Alexander Gayko <a.gayko@googlemail.com>
2022-08-13 15:03:21 +0200

    added BooleanExpressions demo

---
+++ ConsoleTestApp/BooleanExpressions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestApp
+{
+    internal class BooleanExpressions
+    {
+
+        /*
+literal = true | false
+expression = literal | conjunction | disjunction
+conjunction = expression "&&" expression
+disjunction = expression "||" expression
+
+         
+         
+         */
+
+        abstract class Expression
+        {
+            public abstract bool Evaluate();
+
+            public static Expression Parse(string str)
+            {
+                throw new NotImplementedException(); 
+            }
+        }
+
+        class Literal : Expression
+        {
+            public bool Value { get; set; }
+            
+            public override bool Evaluate() => Value;
+
+            public override string ToString() => $"Value";
+        }
+
+        /// <summary>
+        /// A || B
+        /// </summary>
+        class Disjunction : Expression
+        {
+            public Expression A { get; set; }
+            public Expression B { get; set; }
+
+            public override bool Evaluate() => A.Evaluate() || B.Evaluate();
+
+            public override string ToString() => $"({A} || {B})";
+        }
+
+        /// <summary>
+        /// A && B
+        /// </summary>
+        class Conjunction : Expression
+        {
+            public Expression A { get; set; }
+            public Expression B { get; set; }
+
+            public override bool Evaluate() => A.Evaluate() && B.Evaluate();
+
+            public override string ToString() => $"({A} && {B})";
+        }
+
+        public void TestStuff()
+        {
+            // (true || false) && (false || true)
+            var exp = new Conjunction()
+            {
+                A = new Disjunction 
+                {
+                    A = new Literal { Value = true},
+                    B = new Literal { Value = false },
+                } ,
+                B = new Disjunction
+                {
+                    A = new Literal { Value = false },
+                    B = new Literal { Value = true },
+                }
+            };
+
+            var value = exp.Evaluate();
+            var valueStr = exp.ToString(); // ((true || false) && (false || true))
+
+
+            // true && false || true     ==    (true && false) || true
+            var exp2 = new Disjunction
+            {
+                A = new Conjunction
+                {
+                    A = new Literal { Value = true },
+                    B = new Literal { Value = false },
+                },
+                B = new Literal { Value = true },
+            };
+
+            var value2 = exp2.Evaluate();
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
